dab374f56b1963f54832c60814099bba2d80f117
AldoMekic <aldomekic@gmail.com>
2024-12-18 15:20:01 +0100

    Changed the search page results

--- search.php
+++ search.php
@@ -1,33 +1,41 @@
 <?php
-get_header(); 
-?>
-<div class="site-content">
-    <h2><?php printf( __( 'Search Results for: %s', 'mytheme' ), get_search_query() ); ?></h2>
-    
-    <div class="content-sidebar-wrapper">
-        <div class="search-results">
-            <?php if ( have_posts() ) : ?>
-                <div class="posts-list">
-                    <?php while ( have_posts() ) : the_post(); ?>
+get_header(); ?>
+
+<main class="site-main">
+    <div class="content-wrapper"> <!-- Added content-wrapper for consistent layout -->
+
+        <div class="site-content"> <!-- site-content remains here -->
+            <h1><?php printf( __( 'Search Results for: %s', 'mytheme' ), get_search_query() ); ?></h1>
+            <div class="posts-list">
+                <?php if ( have_posts() ) : 
+                    while ( have_posts() ) : the_post(); ?>
                         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                            <h3 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                            <div class="post-meta"><?php the_date(); ?> by <?php the_author(); ?></div>
+                            <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                            <div class="post-meta">
+                                <span><?php the_date(); ?></span> | 
+                                <span><?php the_author(); ?></span>
+                            </div>
                             <div class="post-excerpt">
                                 <?php the_excerpt(); ?>
                             </div>
                         </article>
-                    <?php endwhile; ?>
-                </div>
-                
-                <?php the_posts_navigation(); ?>
-                
-            <?php else : ?>
-                <p><?php _e( 'Sorry, no results found.', 'mytheme' ); ?></p>
-            <?php endif; ?>
-        </div>
+                    <?php endwhile;
+
+                    // Pagination
+                    the_posts_pagination( array(
+                        'mid_size'  => 2,
+                        'prev_text' => __( 'Back', 'mytheme' ),
+                        'next_text' => __( 'Next', 'mytheme' ),
+                    ) );
+                else : ?>
+                    <p><?php _e( 'Sorry, no results found.', 'mytheme' ); ?></p>
+                <?php endif; ?>
+            </div>
+        </div><!-- .site-content -->
+
+        <?php get_sidebar(); ?> <!-- Sidebar placed here to be on the right side -->
         
-        <?php get_sidebar(); ?>
-    </div>
-</div><!-- .site-content -->
-<?php
-get_footer();
+    </div><!-- .content-wrapper -->
+</main>
+
+<?php get_footer(); ?>
